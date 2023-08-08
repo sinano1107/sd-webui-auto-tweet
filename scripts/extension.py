7154d92eb7db44041ae3541aef3f5a792512494e
sinano1107 <tftf161107@icloud.com>
2023-08-08 16:17:28 +0900

    [update] Tweet image

--- scripts/extension.py
+++ scripts/extension.py
@@ -2,45 +2,54 @@ import modules.scripts as scripts
 import gradio as gr
 
 from modules import script_callbacks, errors
-from modules.scripts import PostprocessImageArgs
 from modules.shared import opts, OptionInfo
 
-from tweepy import Client, Unauthorized
+from tweepy import OAuthHandler, API, Client, Unauthorized
+
+autoTweet = False
 
 
-class ExtensionTemplateScript(scripts.Script):
-    def __init__(self) -> None:
-        super().__init__()
-        self.autoTweet = False
+def onChangeCheckbox(value):
+    global autoTweet
+    autoTweet = value
 
+
+class AutoTweetScript(scripts.Script):
     def show(self, _):
         return scripts.AlwaysVisible
 
     def ui(self, _):
         autoTweetCheckbox = gr.Checkbox(False, label="Enable auto tweet")
-        autoTweetCheckbox.change(self.onChangeCheckbox, inputs=autoTweetCheckbox)
+        autoTweetCheckbox.change(onChangeCheckbox, inputs=autoTweetCheckbox)
 
-    def postprocess_image(self, p, pp: PostprocessImageArgs, *args):
-        if self.autoTweet:
-            self.tweet()
-        return super().postprocess_image(p, pp, *args)
 
-    def onChangeCheckbox(self, value):
-        self.autoTweet = value
+def on_image_saved(imageSaveParams: script_callbacks.ImageSaveParams):
+    global autoTweet
+    if autoTweet == False:
+        return
+    data = opts.data
+    try:
+        auth = OAuthHandler(
+            data["consumer_key"],
+            data["consumer_secret"],
+            data["access_token"],
+            data["access_token_secret"],
+        )
+        client = Client(
+            data["bearer_token"],
+            data["consumer_key"],
+            data["consumer_secret"],
+            data["access_token"],
+            data["access_token_secret"],
+        )
+        api = API(auth)
+        media = api.media_upload(imageSaveParams.filename)
+        client.create_tweet(media_ids=[media.media_id])
+    except (KeyError, Unauthorized):
+        errors.report("Please autp-tweet set up.")
 
-    def tweet(self):
-        data = opts.data
-        try:
-            client = Client(
-                data["bearer_token"],
-                data["consumer_key"],
-                data["consumer_secret"],
-                data["access_token"],
-                data["access_token_secret"],
-            )
-            client.create_tweet(text="Hello Twitter!")
-        except (KeyError, Unauthorized):
-            errors.report("Please autp-tweet set up.")
+
+script_callbacks.on_image_saved(on_image_saved)
 
 
 def on_ui_settings():
